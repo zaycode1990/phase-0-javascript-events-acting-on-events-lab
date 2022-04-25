3e38d544ed596459d5ca42436ce6733a83dffd18
Isaiah <ijandicos90@gmail.com>
2022-04-25 12:04:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+
+
+
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
